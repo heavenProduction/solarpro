63433aa036695aeb22a10a9a8f47a29c53c1e206
heavenProduction <satyam382000@gmail.com>
2026-03-07 15:08:20 +0530

    Fix JSX formatting and add missing div elements

--- src/solarpro.jsx
+++ src/solarpro.jsx
@@ -2089,6 +2089,9 @@ const SettingsPage = ({ developer, setDevelopers, dateFormat, setDateFormat, pro
             </div>
           </div>
         </div>
+      </div>
+      </div>
+      </div>
       )}
 
       <div className="mt-4"><Btn onClick={save} size="lg"><Icon name="check" size={16}/>Save All Settings</Btn></div>
